f3c515831f11b073b97a5ed11936a6ba12fea165
florian813 <56045484+florian813@users.noreply.github.com>
2022-01-28 14:19:50 +0100

    update html

--- index.php
+++ index.php
@@ -12,9 +12,52 @@
     </head>
 
     <body id="body" style="background:white ; color=#191A19;">
-        <div>
-            <?php include("pages/navbar.html"); ?>
+       
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <img src="img/pizza.svg "class="navbar-brand" href="#" style="width: 2%;height: 2%; margin-left: 1%;"></img>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+
+          <button onclick="darkFunction()" class="btn btn-secondary" id="light-button" style="margin-left:85%;" >dark mode</button>
         </div>
+            
+      </nav>
+    
+</div>
+                fsdfd
+        <script>
+            function darkFunction() {
+                var body = document.body;
+                
+                if(body.style.background=="white"){
+                    body.style.background="#191A19";
+                    body.style.color="white";
+                    document.getElementById("light-button").textContent="light mode";
+                }else{
+                    body.style.background="white";
+                    body.style.color="#191A19";
+                    document.getElementById("light-button").textContent="dark mode";
+                }
+            }
+        </script>
+
+    </body>
                 fsdfd
         <script>
             function darkFunction() {

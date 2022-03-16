1142ffecb249f617cc96cd92c86f63cdcbc9afc2
florian813 <56045484+florian813@users.noreply.github.com>
2022-03-16 21:04:29 +0100

    Update Model.php

--- src/model/Model.php
+++ src/model/Model.php
@@ -5,11 +5,11 @@ namespace model;
 class Model {
 
   static function connect()
-  {
-    $dsn = "mysql:host=sql4.freesqldatabase.com;dbname=sql4479519;charset=UTF8";
-    $user = "sql4479519";
-    $pass = "Qv34BBTXiL";
-    return new \PDO($dsn, $user, $pass);
-  }
+{
+  $dsn = "mysql:host=sql4.freesqldatabase.com;dbname=sql4479519;charset=UTF8";
+  $user = "sql4406681";
+  $pass = "Qv34BBTXiL";
+  return new \PDO($dsn, $user, $pass);
+}
 
 }
